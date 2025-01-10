d2c5f499e7cc511e3deb787da9ee2d2291354ad3
Nagraj <ntadingi5@gmail.com>
2025-01-10 09:42:17 +0530

    added cursor pointer to theme toggle

--- components/common-components/theme-toggle.jsx
+++ components/common-components/theme-toggle.jsx
@@ -18,7 +18,7 @@ export function ModeToggle() {
 
   return (
     <DropdownMenu>
-      <CustomTooltip content="Change theme to dark or light mode"> 
+      <CustomTooltip content="Change theme to dark or light mode">
         <DropdownMenuTrigger asChild>
           <Button variant="outline" size="icon">
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
@@ -28,13 +28,22 @@ export function ModeToggle() {
         </DropdownMenuTrigger>
       </CustomTooltip>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+        <DropdownMenuItem
+          onClick={() => setTheme("light")}
+          className="cursor-pointer"
+        >
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem
+          onClick={() => setTheme("dark")}
+          className="cursor-pointer"
+        >
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem
+          onClick={() => setTheme("system")}
+          className="cursor-pointer"
+        >
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
