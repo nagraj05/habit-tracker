4221954a4ac912bf78f33339ac112d2d99159077
Nagraj <ntadingi5@gmail.com>
2025-01-21 17:27:05 +0530

    if clicked on header it redirects to landing page

--- components/common-components/Header.jsx
+++ components/common-components/Header.jsx
@@ -2,13 +2,21 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import { ModeToggle } from "./theme-toggle";
+import { useRouter } from "next/navigation";
 
 const ProfilePic = dynamic(() => import("./ProfilePic"), { ssr: false });
 
 export default function Header({ title, buttonName = [] }) {
+  const router = useRouter();
+
+  const handleHome = () => {
+    router.push("/landing")
+  }
+
+
   return (
     <div className="h-16 bg-gray-200 dark:bg-gray-800 flex justify-between items-center px-4">
-      <h1 className="text-lg font-bold tracking-tight dark:text-white">
+      <h1 className="text-lg font-bold tracking-tight dark:text-white cursor-pointer" onClick={handleHome}>
         {title}
       </h1>
       <div className="flex items-center gap-4">
